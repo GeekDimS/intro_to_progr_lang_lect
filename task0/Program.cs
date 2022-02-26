ea8e7fc42edd56128b3a9ee5f0740e27abc12bf9
DimS <dimsed13@yandex.ru>
2022-02-26 10:11:21 +0300

    Первая задача

---
+++ task0/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число => ");// Ввод приглашения для ввода
+string inputValue = Console.ReadLine(); //Ввод строки
+int value = int.Parse(inputValue); //Преобразует строку в целое число
+
+double result = value * value; //Вычисление квадрата
+Console.WriteLine ($"Квадрат числа {value} равен: {result}");
+
+int value = 3;
+
