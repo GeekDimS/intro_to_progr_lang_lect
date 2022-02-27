21dd29ab684914e578c785886803fa6a16c8b269
DimS <dimsed13@yandex.ru>
2022-02-27 11:01:07 +0300

    Задачи до 6

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!"); // Программа проверки является-ли второе число квадратом первого
+Console.WriteLine("Введите первое число: ");
+string value_a = Console.ReadLine();
+int a = int.Parse(value_a);
+Console.WriteLine("Введите второе число: ");
+string value_b = Console.ReadLine();
+int b = int.Parse(value_b);
+
+int result = a*a;
+
+if (b == a*a)
+{
+    Console.WriteLine("Второе число является квадратом первого.");
+}
+else 
+{
+    Console.WriteLine("Второе число не является квадратом первого.");
+}
+
